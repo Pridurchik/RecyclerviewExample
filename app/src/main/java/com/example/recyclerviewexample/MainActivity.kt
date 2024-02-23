d035304c60731e6c3a0b245789972503cfc2dafb
Pridurchik <magomedovgarun02@gmail.com>
2024-02-23 20:45:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/recyclerviewexample/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.recyclerviewexample
+
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewexample.adapter.CustomRecyclerAdapter
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        recyclerView.adapter = CustomRecyclerAdapter(fillList())
+    }
+
+    private fun fillList(): List<String> {
+        val data = mutableListOf<String>()
+        (0..30).forEach { i -> data.add("$i element") }
+        return data
+    }
+
+
+}
